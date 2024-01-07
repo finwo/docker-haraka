038b6a99b67146d87c18557883c7b1d2ad4b8737
Yersa Nordman <yersa@finwo.nl>
2024-01-07 22:29:13 +0100

    Tag freshness detection

--- script/update.sh
+++ script/update.sh
@@ -5,7 +5,11 @@ curl -sL https://api.github.com/repos/haraka/haraka/tags | \
   jq -r '.[]|[.name, .commit.url] | @tsv' | \
   sort | \
   while IFS=$'\t' read -r tag tagurl; do
-    echo "tag   : $tag"
-    echo "tagurl: $tagurl"
+
+    if [[ $(curl -sL $tagurl | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60)  | trunc)") -lt 36 ]]; then
+      echo "tag $tag is fresh"
+    else
+      echo "tag $tag is stale"
+    fi
     echo "---"
   done

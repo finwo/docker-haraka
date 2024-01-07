d18426780268554e5ebd9ebeaeb37bbecddd30d2
Yersa Nordman <yersa@finwo.nl>
2024-01-07 23:33:24 +0100

    Fix missing binary in entrypoint

--- dckr/default/entrypoint.sh
+++ dckr/default/entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -z "$HOSTS" ]; then
   echo "$HOSTS" | tr ',' '\n' > /usr/local/haraka/config/host_list
 fi
 
-exec haraka -c /usr/local/haraka 2>&1
+exec /opt/haraka/bin/haraka -c /opt/haraka 2>&1

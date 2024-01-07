0687d09209f321de0774c38649a666530b0cb848
Yersa Nordman <yersa@finwo.nl>
2024-01-07 22:31:15 +0100

    Attempt destructuring tag into minor and major

--- script/update.sh
+++ script/update.sh
@@ -6,10 +6,14 @@ curl -sL https://api.github.com/repos/haraka/haraka/tags | \
   sort | \
   while IFS=$'\t' read -r tag tagurl; do
 
-    if [[ $(curl -sL $tagurl | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60)  | trunc)") -lt 36 ]]; then
-      echo "tag $tag is fresh"
-    else
-      echo "tag $tag is stale"
+    if [[ $(curl -sL $tagurl | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60)  | trunc)") -gt 36 ]]; then
+      echo "tag $tag is stale, skipping"
+      # continue
     fi
-    echo "---"
+
+
+    minortag=${tag%.*}
+    majortag=${minortag%.*}
+
+    echo "Processing $tag, $minortag, $majortag"
   done

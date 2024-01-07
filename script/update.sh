e778cff3580b16bf59d45fa2226ffb40eec2521c
Yersa Nordman <yersa@finwo.nl>
2024-01-07 22:25:38 +0100

    Sort, so most recent tag is processed last

--- script/update.sh
+++ script/update.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-curl https://api.github.com/repos/haraka/haraka/tags | \
+# Fetch all tags
+curl -sL https://api.github.com/repos/haraka/haraka/tags | \
   jq -r '.[]|[.name, .commit.url] | @tsv' | \
+  sort | \
   while IFS=$'\t' read -r tag tagurl; do
     echo "tag   : $tag"
     echo "tagurl: $tagurl"

c4f9830ec9ceea480c023955c444bf337cd48034
Yersa Nordman <yersa@finwo.nl>
2024-01-07 22:50:20 +0100

    Builddir init

--- script/update.sh
+++ script/update.sh
@@ -1,19 +1,52 @@
 #!/usr/bin/env bash
 
+BASE=$(realpath $(dirname $0)/..)
+BUILDDIR=${BASE}/build
+
+function install_versioned {
+  PATCH=$1
+  MINOR=${TAG%.*}
+  MAJOR=${MINOR%.*}
+  FILENAME=$2
+  mkdir -p $(dirname "${BUILDDIR}/${FILENAME}")
+  if [ -f "${BASE}/dckr/${PATCH}/${FILENAME}" ]; then
+    cp -r "${BASE}/dckr/${PATCH}/${FILENAME}" "${BUILDDIR}/${FILENAME}"
+  elif [ -f "${BASE}/dckr/${MINOR}/${FILENAME}" ]; then
+    cp -r "${BASE}/dckr/${MINOR}/${FILENAME}" "${BUILDDIR}/${FILENAME}"
+  elif [ -f "${BASE}/dckr/${PATCH}/${FILENAME}" ]; then
+    cp -r "${BASE}/dckr/${PATCH}/${FILENAME}" "${BUILDDIR}/${FILENAME}"
+  elif [ -f "${BASE}/dckr/default/${FILENAME}" ]; then
+    cp -r "${BASE}/dckr/default/${FILENAME}" "${BUILDDIR}/${FILENAME}"
+  else
+    echo "${FILENAME} could not be found" >&2
+    exit 1
+  fi
+}
+
 # Fetch all tags
 curl -sL https://api.github.com/repos/haraka/haraka/tags | \
   jq -r '.[]|[.name, .commit.url] | @tsv' | \
   sort | \
   while IFS=$'\t' read -r tag tagurl; do
 
-    if [[ $(curl -sL $tagurl | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60)  | trunc)") -gt 36 ]]; then
+    # Skip tag if it's older than 36 hours
+    # Will push a tag twice, should fix later
+    if [[ $(curl -sL $tagurl | tee /dev/stderr | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60)  | trunc)") -gt 36 ]]; then
       echo "tag $tag is stale, skipping"
       # continue
     fi
 
+    # # Destructure tag into major/minor/patch
+    # minortag=${tag%.*}
+    # majortag=${minortag%.*}
 
-    minortag=${tag%.*}
-    majortag=${minortag%.*}
+    # Reset build dir
+    rm -rf ${BUILDDIR}
+    mkdir -p ${BUILDDIR}
+    install_versioned ${tag} Dockerfile
+    install_versioned ${tag} haraka.sh
 
-    echo "Processing $tag, $minortag, $majortag"
+    tree ${BUILDDIR}
+
+    echo "Processing $tag"
   done

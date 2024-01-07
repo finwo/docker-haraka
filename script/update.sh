a46e400ee422d48bf77a7be1395da5c8ef5dca29
Yersa Nordman <yersa@finwo.nl>
2024-01-07 22:22:39 +0100

    Fixed forgotten pipe character

--- script/update.sh
+++ script/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl https://api.github.com/repos/haraka/haraka/tags | \
-  jq -r '.[]|[.name, .commit.url] | @tsv' \
+  jq -r '.[]|[.name, .commit.url] | @tsv' | \
   while IFS=$'\t' read -r tag tagurl; do
     echo "tag   : $tag"
     echo "tagurl: $tagurl"
